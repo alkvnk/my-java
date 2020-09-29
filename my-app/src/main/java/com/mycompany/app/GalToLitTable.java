d518accaabae45eb58704d8c238062bba293c1b3
Alexey <alkvnk@gmail.com>
2020-09-29 12:13:30 +0000

    run Java app in docker container

---
+++ my-app/src/main/java/com/mycompany/app/GalToLitTable.java
@@ -0,0 +1,26 @@
+package com.mycompany.app;
+
+/**
+ *
+ * @author alexey.vankov
+ */
+public class GalToLitTable {
+    public static void main(String[] args) {
+      
+        double gallons,liters;  // TODO code application logic here
+        int    counter;
+       
+        counter =0;
+        for (gallons = 1; gallons <16; gallons++){
+        liters = gallons*3.7854;
+        System.out.println(gallons + " gallons equals " 
+                                   + liters + " liters");
+        counter++;
+            if (counter == 5) {
+            System.out.println();
+            counter=0;
+            }
+        }
+    }
+    
+}
